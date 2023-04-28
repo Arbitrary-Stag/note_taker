1d459c6a115f21dd2c15f4c90a84b069e8cbefe6
Arbitrary Stag <will.weaver.iv@gmail.com>
2023-04-27 18:16:23 -0700

    cleaned up server.js code

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-// Import Express.js
 const express = require('express');
-
-// Import built-in Node.js package 'path' to resolve path of files that are located on the server
 const path = require('path');
-
-// Initialize an instance of Express.js
-const app = express();
-
-// Specify on which port the Express.js server will run
+const notes = require('./db/db.json');
 const PORT = 3001;
 
-// Static middleware pointing to the public folder
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static('public'));
 
-// Create Express.js routes for default '/', '/send' and '/routes' endpoints
 app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 
 app.get('/send', (req, res) =>
@@ -24,7 +20,4 @@ app.get('/routes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/routes.html'))
 );
 
-// listen() method is responsible for listening for incoming connections on the specified port 
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
+app.listen(PORT);
